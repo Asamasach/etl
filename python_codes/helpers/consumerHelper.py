3dde90e99441016fadc642673820e1e650259cb1
Mehdi Hosseini <m.hosseini@dataak.com>
2020-04-28 12:17:17 +0430

    add enable switch

--- python_codes/helpers/consumerHelper.py
+++ python_codes/helpers/consumerHelper.py
@@ -19,6 +19,7 @@ class Consumer():
         consumers = mysql.get_list(group_id = self.consumer_group_id)
         all_process = 0
         if self.old_consumers != consumers:
+        
 
             all_process=0
             jobs = []
@@ -27,27 +28,30 @@ class Consumer():
                     self.p.terminate()
 
             for consumer in consumers:
-                kafka_worker = []
-                print(consumer)
-                for i in range(int(consumer[6])):
-                    all_process+=1
-                    kafka_worker.append(Kafka(
-                                    topic_name = consumer[1],
-                                    group_id = consumer[0],
-                                    auto_offset_reset = consumer[5]
-                                    ))
-                    
-                for kafka_object in kafka_worker:
-                    self.p = multiprocessing.Process(target=self.elasticsearch_instance.post(
-                                                                                            kafka_object.consume(),
-                                                                                            consumer[2]) 
-                                                                                            )  # consumer[2] : consumer['elastic_index']
-                    jobs.append(self.p)
-                    self.p.start()
-                
-                print(all_process)
-                self.old_consumers = consumer 
+                if str(consumer[7]) == '1':
 
+                    kafka_worker = []
+                    print(consumer)
+                    for i in range(int(consumer[6])):
+                        all_process+=1
+                        kafka_worker.append(Kafka(
+                                        topic_name = consumer[1],
+                                        group_id = consumer[0],
+                                        auto_offset_reset = consumer[5]
+                                        ))
+                        
+                    for kafka_object in kafka_worker:
+                        self.p = multiprocessing.Process(target=self.elasticsearch_instance.post(
+                                                                                                kafka_object.consume(),
+                                                                                                consumer[2]) 
+                                                                                                )  # consumer[2] : consumer['elastic_index']
+                        jobs.append(self.p)
+                        self.p.start()
+                    
+                    print(all_process)
+                    self.old_consumers = consumer 
+                    else:
+                        print("consumer is disabled!")
         else:
             time.sleep(10)
          
