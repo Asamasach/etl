7cbc09e2e50cf08a8aa27153a364205b40a48721
Mehdi Hosseini <m.hosseini@dataak.com>
2020-04-29 16:17:12 +0430

    merge manager to consumerHelper

--- python_codes/helpers/consumerHelper.py
+++ python_codes/helpers/consumerHelper.py
@@ -1,67 +1,73 @@
 from kafkaHelper import Kafka
 from mysqlHelper import Mysql
-
 from logHelper import Log
 from multiprocessing import Process
 import time
 
-class Consumer():
 
-    def __init__(self, consumer_group_id):
-        self.consumer_group_id = consumer_group_id
 
-        self.old_consumers = []
-        self.p = []
+#         self.old_consumers = []
+#         self.p = []
         
-#        self.log = Log("consumerHelper")
-        self.jobs = []
+# #        self.log = Log("consumerHelper")
+#         self.jobs = []
 
-    def consume(self):
 
-        consumers = []
-        mysql = Mysql()
-        consumers = mysql.get_list(group_id = self.consumer_group_id)
-        all_process = 0
-        if self.old_consumers != consumers:
-        
+mysql_instance = Mysql()
+all_process = 0
 
-            all_process=0
-#            jobs = []
-            if self.jobs:
-                print("we have processes which going to terminate")
-                for self.p in self.jobs: 
-                    self.p.terminate()
-            
-                self.jobs = []
-            for consumer in consumers:
-                if str(consumer[7]) == '1':
+old_groups = []
+old_consumers = []
+consumers_group = []
+jobs = []
 
-                    kafka_worker = []
-                    print(consumer)
-                    for _ in range(int(consumer[6])):
-                        all_process+=1
-                        kafka_worker.append(Kafka(
-                                        topic_name = consumer[1],
-                                        group_id = consumer[0],
-                                        auto_offset_reset = consumer[5]
-                                        ))
-                    print("consumer_id {} is running now".format(consumer[3])) 
-                       
-                    for kafka_object in kafka_worker:
-                        self.p = Process(target=kafka_object.consume(consumer[2], consumer[3]))   # consumer[2] : consumer['elastic_index'] # id
-                        self.jobs.append(self.p)
-                    print("number of running process are : {}".format(all_process))
-                else:
-                    self.log.write("consumer_id {} is disabled".format(consumer[3]),"consumer")
-            for self.p in self.jobs:
-                self.p.start()
-                self.p.join()
 
-            self.old_consumers = consumers
-        else:
-            time.sleep(1)
+if __name__ == "__main__":
 
-        return all_process
-    # def elk_consume(self, data, index):
+    groups = mysql_instance.get_groups()
+    if old_groups != groups:
+        for i in range(len(groups)):
+            group_id = groups[i][0]
+            print("(group_id: {} ) has been detected!".format(group_id)) #  .format(group_id)
+            consumers = mysql_instance.get_list(group_id = group_id)
+            if old_consumers != consumers:
+                all_process=0
+                if jobs:
+                    print("we have {} processes which are going to be terminated".format(self.jobs))
+                    for p in jobs: 
+                        p.terminate()
+                    jobs = []
 
-        # return None
+
+                for consumer in consumers:
+                    if str(consumer[7]) == '1':
+
+                        kafka_worker = []
+                        print(consumer)
+                        for _ in range(int(consumer[6])):
+                            all_process+=1
+                            kafka_worker.append(Kafka(
+                                            topic_name = consumer[1],
+                                            group_id = consumer[0],
+                                            auto_offset_reset = consumer[5]
+                                            ))
+                        print("consumer_id {} is running now".format(consumer[3])) 
+                    
+                        for kafka_object in kafka_worker:
+                            p = Process(target=kafka_object.consume(consumer[2], consumer[3]))   # consumer[2] : consumer['elastic_index'] # id
+                            jobs.append(p)
+                        print("number of running process are : {}".format(all_process))
+                    else:
+                        print("consumer_id {} is disabled".format(consumer[3])) #,"consumer")
+                for p in jobs:
+                    p.start()
+                    p.join()
+
+                old_consumers = consumers
+            else:
+                time.sleep(1)
+                        
+        old_groups = groups
+
+    else:
+        time.sleep(1)
