91c520491e9822f5f625fe4a6573103fbe1daf64
root <root@kibana.asamasach.ir>
2020-04-29 17:42:13 +0430

    refactor manager

--- python_codes/helpers/kafkaHelper.py
+++ python_codes/helpers/kafkaHelper.py
@@ -11,7 +11,7 @@ config_file_path = os.path.join(fileDir,  "config/kafka.yml")
 
 class Kafka():
 
-    def __init__(self, topic_name, group_id, auto_offset_reset):
+    def __init__(self, topic_name, group_id, auto_offset_reset, kafka_id):
         
         with open(config_file_path) as kafka_conf:
            self.conf = yaml.load(kafka_conf, Loader=yaml.FullLoader)
@@ -21,7 +21,7 @@ class Kafka():
         self.topic_name = topic_name
         self.auto_offset_reset = auto_offset_reset
         self.running_consumer = True
-
+        self.kafka_id = kafka_id
         self.c = Consumer({
                     'bootstrap.servers': self.conf['bootstrap_servers'],
                     'group.id': self.group_id,
--- python_codes/helpers/manager.py
+++ python_codes/helpers/manager.py
@@ -1,28 +1,60 @@
+from kafkaHelper import Kafka
 from mysqlHelper import Mysql
 from logHelper import Log
-from consumerHelper import Consumer
+from multiprocessing import Process
 import time
+
+mysql_instance = Mysql()
+process_id = 0
 
 old_groups = []
 old_consumers = []
 consumers_group = []
-mysql_instance = Mysql()
-manager_log = Log('notification')
+jobs = []
 
-if __name__ == "__main__":
 
-    groups = mysql_instance.get_groups()
+while True:
 
-    if old_groups != groups:
-        for i in range(len(groups)):
-            group_id = groups[i][0]
-            consumers_group.append(Consumer(group_id))
-            message = "new group_id has been detected!" #  .format(group_id)
-            print(message)
-            #manager_log.write(message,"Manager")
-        old_groups = groups
+        consumers = mysql_instance.get_list()
+        if old_consumers != consumers:
+            groups = mysql_instance.get_groups()
+            for i in range(len(groups)):
+                group_id = groups[i][0]
+                print("(group_id: {} ) has been detected!".format(group_id))
+            process_id=0
+            if jobs:
+                print("we have {} processes which are going to be terminated".format(jobs))
+                for p in jobs: 
+                    p.terminate()
+                jobs = []
 
-    while old_groups == groups: 
-        for consumer in consumers_group:
-            consumer.consume()
-           # time.sleep(1)
+
+            for consumer in consumers:
+                if str(consumer[7]) == '1':
+
+                    kafka_workers = []
+                    print(consumer)
+                    for _ in range(int(consumer[6])):
+                        kafka_workers.append(Kafka(
+                                        topic_name = consumer[1],
+                                        group_id = consumer[0],
+                                        auto_offset_reset = consumer[5],
+                                        kafka_id = process_id
+                                        ))
+                        print("{}th process of consumer_id {} added to jobs".format(process_id, consumer[3])) 
+                        process_id+=1
+                
+                    for kafka_worker in kafka_workers:
+                        p = Process(target=kafka_worker.consume(consumer[2], consumer[3]))   # consumer[2] : consumer['elastic_index'] # id
+                        jobs.append(p)
+                    print("number of running process are : {}".format(process_id))
+                    for p in jobs:
+                        p.start()
+                        p.join()
+
+                else:
+                    print("consumer_id {} is disabled".format(consumer[3])) #,"consumer")
+
+            old_consumers = consumers
+        else:
+            time.sleep(1)
