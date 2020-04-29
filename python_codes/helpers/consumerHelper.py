5134fe145837fb0cabaf2b3f44b4cd2d77815ddb
Mehdi Hosseini <m.hosseini@dataak.com>
2020-04-29 16:47:21 +0430

    modify consumerHelper

--- python_codes/helpers/consumerHelper.py
+++ python_codes/helpers/consumerHelper.py
@@ -4,17 +4,8 @@ from logHelper import Log
 from multiprocessing import Process
 import time
 
-
-
-#         self.old_consumers = []
-#         self.p = []
-        
-# #        self.log = Log("consumerHelper")
-#         self.jobs = []
-
-
 mysql_instance = Mysql()
-all_process = 0
+process_id = 0
 
 old_groups = []
 old_consumers = []
@@ -22,52 +13,48 @@ consumers_group = []
 jobs = []
 
 
-if __name__ == "__main__":
+while True:
 
-    groups = mysql_instance.get_groups()
-    if old_groups != groups:
-        for i in range(len(groups)):
-            group_id = groups[i][0]
-            print("(group_id: {} ) has been detected!".format(group_id)) #  .format(group_id)
-            consumers = mysql_instance.get_list(group_id = group_id)
-            if old_consumers != consumers:
-                all_process=0
-                if jobs:
-                    print("we have {} processes which are going to be terminated".format(self.jobs))
-                    for p in jobs: 
-                        p.terminate()
-                    jobs = []
+        consumers = mysql_instance.get_list()
+        if old_consumers != consumers:
+            groups = mysql_instance.get_groups()
+            for i in range(len(groups)):
+                group_id = groups[i][0]
+                print("(group_id: {} ) has been detected!".format(group_id))
+            process_id=0
+            if jobs:
+                print("we have {} processes which are going to be terminated".format(jobs))
+                for p in jobs: 
+                    p.terminate()
+                jobs = []
 
 
-                for consumer in consumers:
-                    if str(consumer[7]) == '1':
+            for consumer in consumers:
+                if str(consumer[7]) == '1':
 
-                        kafka_worker = []
-                        print(consumer)
-                        for _ in range(int(consumer[6])):
-                            all_process+=1
-                            kafka_worker.append(Kafka(
-                                            topic_name = consumer[1],
-                                            group_id = consumer[0],
-                                            auto_offset_reset = consumer[5]
-                                            ))
-                        print("consumer_id {} is running now".format(consumer[3])) 
-                    
-                        for kafka_object in kafka_worker:
-                            p = Process(target=kafka_object.consume(consumer[2], consumer[3]))   # consumer[2] : consumer['elastic_index'] # id
-                            jobs.append(p)
-                        print("number of running process are : {}".format(all_process))
-                    else:
-                        print("consumer_id {} is disabled".format(consumer[3])) #,"consumer")
-                for p in jobs:
-                    p.start()
-                    p.join()
+                    kafka_workers = []
+                    print(consumer)
+                    for _ in range(int(consumer[6])):
+                        kafka_workers.append(Kafka(
+                                        topic_name = consumer[1],
+                                        group_id = consumer[0],
+                                        auto_offset_reset = consumer[5],
+                                        kafka_id = process_id
+                                        ))
+                        print("{}th process of consumer_id {} added to jobs".format(process_id, consumer[3])) 
+                        process_id+=1
+                
+                    for kafka_worker in kafka_workers:
+                        p = Process(target=kafka_worker.consume(consumer[2], consumer[3]))   # consumer[2] : consumer['elastic_index'] # id
+                        jobs.append(p)
+                    print("number of running process are : {}".format(process_id))
+                    for p in jobs:
+                        p.start()
+                        p.join()
 
-                old_consumers = consumers
-            else:
-                time.sleep(1)
-                        
-        old_groups = groups
+                else:
+                    print("consumer_id {} is disabled".format(consumer[3])) #,"consumer")
 
-    else:
-        time.sleep(1)
+            old_consumers = consumers
+        else:
+            time.sleep(1)
